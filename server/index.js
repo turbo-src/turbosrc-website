46b9fc1fde6518c17e1e8ad9e505f654dbcf3e62
Jeffrey Wood <jwfirenze@gmail.com>
2022-09-20 13:08:08 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+
+const app = require("./app");
+
+app.listen(port, () => {
+  console.log(`turbo-src website listening on port ${port}`);
+});
