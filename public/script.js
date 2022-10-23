9db3738edbc6fe7d7349be65ee90556c01b2325d
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-23 17:29:41 -0400

    waitlist functional

---
+++ public/script.js
@@ -0,0 +1,22 @@
+(function () {
+    emailjs.init("QjpzE2RJb1rGVGPs0");
+  })();
+  
+  window.onload = function () {
+    document
+      .getElementById("email-list")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+        emailjs.sendForm("service_36ferie", "template_bm4bbbe", this).then(
+          function () {
+            document.getElementById("email-input").value = "Thank you!";
+          },
+          function (error) {
+            document.getElementById("confirm-email").innerText =
+              "There was an error.";
+            console.log(error);
+          }
+        );
+      });
+  };
+  
