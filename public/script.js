7a77b0bc2ca50e377dd29f155b16f09a48f5e32f
Jeffrey Wood <jwfirenze@gmail.com>
2022-10-26 13:38:38 -0400

    or spacing

--- public/script.js
+++ public/script.js
@@ -9,7 +9,7 @@
         event.preventDefault();
         emailjs.sendForm("service_36ferie", "template_bm4bbbe", this).then(
           function () {
-            document.getElementById("email-input").value = "Thank you!";
+            document.getElementById("email-input").value = "We'll contact you soon!";
           },
           function (error) {
             document.getElementById("confirm-email").innerText =
